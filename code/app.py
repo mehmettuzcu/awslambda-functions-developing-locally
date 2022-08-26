e215ce4849943f5158ba7ef9fc980c59b0b017d7
mehmettuzcu22 <mehmettuzcu22@gmail.com>
2022-08-26 16:01:58 +0300

    Data uploaded to database

--- code/app.py
+++ code/app.py
@@ -1,24 +1,60 @@
 import pandas as pd
 import requests
 import json
+from sqlalchemy import create_engine
+import sqlalchemy
+from datetime import date, datetime, timedelta
+import os
 
 def wheather(event, context):
+
+    begin = datetime.now()
+    print(f"""
+    ##############################################
+    Start Time: {begin}
+    """)
 
     first_name = event["first_name"]
     second_name = event["second_name"]
     message = event["message"]
+    user = os.environ.get('USER')
+    passwd = os.environ.get('PASSWORD')
+    host = os.environ.get('HOST')
+    port = os.environ.get('PORT')
+    db = os.environ.get('DATABASE')
+
+    # MySQL conection string.
+    creds = {'user': user,
+            'passwd': passwd,
+            'host': host,
+            'port': port,
+            'db': db}
+    connstr = 'mysql+mysqlconnector://{user}:{passwd}@{host}:{port}/{db}'
+    mysql_conn = create_engine(connstr.format(**creds))
+    # print(pd.read_sql_query(sql='SHOW TABLES', con=mysql_conn))
+
     params = {
       'ippd': 'weather.com',
     }
 
     response = requests.get('https://securepubads.g.doubleclick.net/pagead/ppub_config', params=params)
     data = response.json()
+
     id = []
     for l in data[0][3]:
         id.append(l[1])
 
     df = pd.DataFrame(id)
-    print(df)
+    df.to_sql(name='wheatherId',con=mysql_conn, index=False, if_exists='replace')
+    print("Data Loaded Succesfully")
+
+    end = datetime.now()
+    print(f"""
+    Finished Time: {end}
+    Total Time: {end - begin}    
+    ##################################################    
+        """)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
---
+++ y
@@ -0,0 +1,12 @@
+version = 0.1
+[y]
+[y.deploy]
+[y.deploy.parameters]
+stack_name = "ingest"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1ugblzamduso5"
+s3_prefix = "ingest"
+region = "eu-west-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
+disable_rollback = true
+image_repositories = []
