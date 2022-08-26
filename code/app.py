bc68d0eeb45171d26685d04e59d1f1a65867d211
mehmettuzcu22 <mehmettuzcu22@gmail.com>
2022-08-26 17:11:06 +0300

    Data load configurate added for s3 bucket

--- code/app.py
+++ code/app.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import requests
 import json
-from sqlalchemy import create_engine
-import sqlalchemy
-from datetime import date, datetime, timedelta
 import os
+import boto3
+import sqlalchemy
+
+from sqlalchemy import create_engine
+from boto3.session import Session
+from datetime import date, datetime, timedelta
+
 
 def wheather(event, context):
 
@@ -14,6 +18,7 @@ def wheather(event, context):
     Start Time: {begin}
     """)
 
+    # Environment variables
     first_name = event["first_name"]
     second_name = event["second_name"]
     message = event["message"]
@@ -22,6 +27,9 @@ def wheather(event, context):
     host = os.environ.get('HOST')
     port = os.environ.get('PORT')
     db = os.environ.get('DATABASE')
+    access_key = os.environ.get('ACCESS_KEY')
+    secret_key = os.environ.get('SECRET_KEY')
+    bucket_name = os.environ.get('BUCKET_NAME')
 
     # MySQL conection string.
     creds = {'user': user,
@@ -33,10 +41,15 @@ def wheather(event, context):
     mysql_conn = create_engine(connstr.format(**creds))
     # print(pd.read_sql_query(sql='SHOW TABLES', con=mysql_conn))
 
+    # S3
+    session = Session(aws_access_key_id=access_key,
+                  aws_secret_access_key=secret_key)
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
     params = {
       'ippd': 'weather.com',
     }
-
     response = requests.get('https://securepubads.g.doubleclick.net/pagead/ppub_config', params=params)
     data = response.json()
 
@@ -46,7 +59,14 @@ def wheather(event, context):
 
     df = pd.DataFrame(id)
     df.to_sql(name='wheatherId',con=mysql_conn, index=False, if_exists='replace')
-    print("Data Loaded Succesfully")
+    print("Data Loaded Succesfully to MySQL")
+
+    df.to_csv("wheather.csv", index=False)
+    local_path = './wheather.csv'
+    bucket_path = 'tuzcu2/wheather.csv'
+    s3.meta.client.upload_file(Filename=local_path, Bucket=bucket_name, Key=bucket_path)
+
+    print("Data Loaded Succesfully to MySQL")
 
     end = datetime.now()
     print(f"""
--- y
+++ y
@@ -10,3 +10,15 @@ confirm_changeset = true
 capabilities = "CAPABILITY_IAM"
 disable_rollback = true
 image_repositories = []
+
+[default]
+[default.deploy]
+[default.deploy.parameters]
+stack_name = "ingest"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1ugblzamduso5"
+s3_prefix = "ingest"
+region = "eu-west-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
+disable_rollback = true
+image_repositories = []
